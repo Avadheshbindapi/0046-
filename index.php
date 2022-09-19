2b7940a88a83431257ea57f00ab2412beb83d531
Avadheshbindapi <112742743+Avadheshbindapi@users.noreply.github.com>
2022-09-19 11:56:10 +0530

    init commit

--- index.php
+++ index.php
@@ -10,22 +10,22 @@
     <div>
         <form action="" method="POST">
             <input type="text" name="first_name" placeholder="first Name"><br><br>
-            <input type="text" name="last_name" placeholder="Last Name"><br><br>
+            <input type="text" name="second_name" placeholder="second Name"><br><br>
             <input type="submit" name="submit" value="submit"><br><br>
         </form>
         <h2>hellow
-        Hello {first_name} {last_name}! Greetings from BindAPI.
+        Hello {first_name} {second_name}! Greetings from BindAPI.
         </h2>
         <?php
             
              if(isset($_POST['submit']))
              {
                  $first_name = $_POST['first_name'];
-                 $last_name = $_POST['last_name'];
+                 $last_name = $_POST['second_name'];
              }
              echo "$first_name";
                 echo "<br>";
-                echo "$last_name";
+                echo "$second_name";
              
              ?>
             
