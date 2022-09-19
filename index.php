087da519fcbb172841a4bfcf2a7ba60703bf6aec
Avadheshbindapi <112742743+Avadheshbindapi@users.noreply.github.com>
2022-09-19 14:14:42 +0530

    0058school commit

--- index.php
+++ index.php
@@ -1,3 +1,38 @@
+<?php
+    $server = "localhost";
+    $user = "root";
+    $dbname="dev4";
+    $pass = "";
+?>
+    <?php
+    $customer = new Master ($server, $user, $dbname, $pass);
+    if(isset($_POST['submit']))
+    {
+        $first_name = $_POST['first_name'];
+        $second_name = $_POST['second_name'];
+        $mobile = $_POST['mobile'];
+
+        $customer->addlist($first_name, $second_name, $mobile);
+    }
+    ?>
+
+    <?php
+        class Master
+        {
+            public $conn;
+            public function __construct($server, $user, $dbname, $pass)
+            {
+                $this->conn = new PDO("mysql:host=$server;dbname=$dbname", $user, $pass);
+            }
+            public function addlist($first_name, $second_name, $mobile)
+            {
+                $query = "INSERT INTO school VALUES (NULL, ?, ?, ?)";
+                $stmt = $this->conn->prepare($query);
+                $stmt->execute([$first_name, $second_name, $mobile]);
+            }
+        }
+    ?>    
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -7,22 +42,13 @@
     <title>Document</title>
 </head>
 <body>
-    <div>
+    
         <form action="" method="POST">
             <input type="text" name="first_name" placeholder="first Name"><br><br>
             <input type="text" name="second_name" placeholder="second Name"><br><br>
+            <input type="text" name="mobile" placeholder="mobile"><br><br>
             <input type="submit" name="submit" value="submit"><br><br>
         </form>
     
-        <?php
-            
-             if(isset($_POST['submit']))
-             {
-                 $first_name = $_POST['first_name'];
-                 $second_name = $_POST['second_name'];
-                 echo "<h2>Hello $first_name $second_name ! Greetings from BindAPI.</h2>";
-             }  
-             ?> 
-    </div>
 </body>
 </html>
